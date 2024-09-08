ab714c01b299abbcf3c8ccd3d8b36d1bf2798bec
JeanSilva08 <jeancarlossoares.s@gmail.com>
2024-09-08 19:47:40 -0300

    fix: ajustando variavem recinto em verifdicaRegrasEspecifica

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -65,19 +65,21 @@ class RecintosZoo {
 
   verificaRegrasEspecificas(recinto, animal) {
     let regraValida = false;
-
+  
     if (['LEAO', 'LEOPARDO', 'CROCODILO'].includes(animal)) {
-        regraValida = recinto.animais.every(a => a.especie === animal);
+      regraValida = recinto.animais.every(a => a.especie === animal);
     } else if (animal === 'HIPOPOTAMO') {
-        regraValida = recinto.bioma === 'savana e rio' || recinto.animais.every(a => a.especie === 'HIPOPOTAMO');
+      regraValida = recinto.bioma === 'savana e rio' || recinto.animais.every(a => a.especie === 'HIPOPOTAMO');
     } else if (animal === 'MACACO') {
-        regraValida = !recinto.animais.some(a => ['LEAO', 'LEOPARDO', 'CROCODILO'].includes(a.especie));
+      regraValida = !recinto.animais.some(a => ['LEAO', 'LEOPARDO', 'CROCODILO'].includes(a.especie));
     } else {
-        regraValida = true;
+      regraValida = true;
     }
-
+  
     return regraValida;
   }
+  
+  
 
   calculaEspacoLivre(recinto, animal, quantidade) {
     let espacoOcupado = recinto.animais.reduce((acc, animalExistente) => {
